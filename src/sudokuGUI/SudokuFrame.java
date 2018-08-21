9e5079523045f0d4510c48524396458621cd4249
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-20 22:17:18 -0400

    Make button panel, add it to sudoku frame

--- src/sudokuGUI/SudokuFrame.java
+++ src/sudokuGUI/SudokuFrame.java
@@ -1,13 +1,21 @@
 package sudokuGUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SudokuFrame {
     private JFrame frame = new JFrame("Sudoku Solver");
+    private JPanel buttonPanel;
+    private JButton solveButton;
+    private JButton clearButton;
 
     public SudokuFrame() {
+
+        frame.getContentPane().setLayout(new BorderLayout());
+        makeButtons();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new SudokuGrid(3, 3));
+        frame.add(new SudokuGrid(3, 3), BorderLayout.NORTH);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
         createMenu();
         frame.pack();
         frame.setResizable(false);
@@ -19,10 +27,27 @@ public class SudokuFrame {
         JMenu fileMenu = new JMenu("File");
 
         JMenuItem open = new JMenuItem("Open");
+        JMenuItem generate = new JMenuItem("Generate Puzzle");
 
         fileMenu.add(open);
+        fileMenu.addSeparator();
+        fileMenu.add(generate);
         menuBar.add(fileMenu);
 
+
         frame.setJMenuBar(menuBar);
+    }
+
+    private void makeButtons() {
+        this.buttonPanel = new JPanel(new BorderLayout());
+        this.solveButton = new JButton("Solve");
+        this.clearButton = new JButton("Clear");
+
+        Font buttonFont = new Font("sanserif", Font.CENTER_BASELINE, 20);
+        solveButton.setFont(buttonFont);
+        clearButton.setFont(buttonFont);
+
+        buttonPanel.add(solveButton, BorderLayout.WEST);
+        buttonPanel.add(clearButton, BorderLayout.EAST);
     }
 }
