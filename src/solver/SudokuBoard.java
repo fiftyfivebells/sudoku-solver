8d858b16aecb1bd25c9b719227b7e7019e8856ce
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-23 19:30:12 -0400

    Make a SudokuBoard class to represent the board

--- src/solver/SudokuBoard.java
+++ src/solver/SudokuBoard.java
@@ -1,4 +1,25 @@
 package solver;
 
 public class SudokuBoard {
+    private int[][] grid;
+
+    public SudokuBoard(int rows, int cols) {
+        grid = new int[rows][cols];
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public int getDigitAtSquare(int row, int col) {
+        return grid[row][col];
+    }
+
+    public void setDigitAtSquare(int row, int col, int val) {
+        grid[row][col] = val;
+    }
+
+    public void setSquareToDefault(int row, int col) {
+        grid[row][col] = 0;
+    }
 }
